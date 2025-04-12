38539c9a7f4d2ead9f648450cc1e87f321174cf8
Yumin Chen <yumin@chen.so>
2025-04-12 10:15:35 +0100

    Initial commit

---
+++ components/Button/index.tsx
@@ -0,0 +1,31 @@
+import type { ConditionalValue } from "@chen/design-system/types";
+import type { PropsWithChildren } from "react";
+import { Tooltip } from "../Tooltip";
+import {
+	type ButtonProps as BaseButtonProps,
+	Button as ButtonBase,
+} from "./button";
+
+export interface ButtonProps extends PropsWithChildren<BaseButtonProps> {
+	variant?: ConditionalValue<
+		"link" | "outline" | "solid" | "ghost" | "subtle"
+	> | null;
+	tooltip?: string | null;
+	size?: ConditionalValue<"xs" | "sm" | "md" | "lg" | "xl" | "2xl"> | null;
+	children?: React.ReactNode | null;
+}
+
+export const Button = ({ tooltip, children, ...props }: ButtonProps) => {
+	if (tooltip) {
+		return (
+			<Tooltip
+				trigger={<ButtonBase {...props}>{children}</ButtonBase>}
+				content={tooltip}
+			/>
+		);
+	}
+
+	return <ButtonBase {...props}>{children}</ButtonBase>;
+};
+
+export * from "./button";
---
+++ components/Dialog/index.tsx
@@ -0,0 +1,76 @@
+import { Stack } from "@chen/design-system/jsx";
+import { Button } from "../Button";
+import { Dialog as DialogBase } from "./dialog";
+
+const defaultConfirmButton = <Button>Confirm</Button>;
+const defaultCancelButton = <Button variant="outline">Cancel</Button>;
+const defaultCloseButton = (
+	<Button aria-label="Close" variant="ghost" size="sm">
+		{"×"}
+	</Button>
+);
+
+export type DialogProps = DialogBase.RootProps & {
+	trigger?: React.ReactNode | null;
+	title?: React.ReactNode | string | null;
+	description?: React.ReactNode | string | null;
+	confirmButton?: React.ReactNode | null;
+	cancelButton?: React.ReactNode | null;
+	closeButton?: React.ReactNode | null;
+	context?: ((context: unknown) => React.ReactNode) | null;
+};
+
+export const Dialog = ({
+	trigger,
+	title,
+	description,
+	confirmButton = defaultConfirmButton,
+	cancelButton = defaultCancelButton,
+	closeButton = defaultCloseButton,
+	context = undefined,
+	lazyMount = true,
+	...props
+}: DialogProps) => {
+	return (
+		<DialogBase.Root {...props} lazyMount>
+			{trigger && <DialogBase.Trigger asChild>{trigger}</DialogBase.Trigger>}
+			<DialogBase.Backdrop />
+			<DialogBase.Positioner>
+				<DialogBase.Content>
+					<Stack gap="8" p="6">
+						<Stack gap="1">
+							<DialogBase.Title>{title}</DialogBase.Title>
+							<DialogBase.Description>{description}</DialogBase.Description>
+						</Stack>
+						<Stack
+							gap="3"
+							direction="row"
+							width="full"
+							justifyContent="flex-end"
+						>
+							{confirmButton}
+							{cancelButton && (
+								<DialogBase.CloseTrigger asChild>
+									{cancelButton}
+								</DialogBase.CloseTrigger>
+							)}
+						</Stack>
+					</Stack>
+					{closeButton && (
+						<DialogBase.CloseTrigger
+							asChild
+							position="absolute"
+							top="2"
+							right="2"
+						>
+							{closeButton}
+						</DialogBase.CloseTrigger>
+					)}
+				</DialogBase.Content>
+			</DialogBase.Positioner>
+			{context && <DialogBase.Context>{context}</DialogBase.Context>}
+		</DialogBase.Root>
+	);
+};
+
+export * from "./dialog";
